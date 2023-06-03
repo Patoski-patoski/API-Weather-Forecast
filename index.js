36ffe629c3129f6b2a8a0d293a549038bf488eef
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-06-03 23:55:33 +0100

    added api functionalities to the description section

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const main = document.querySelector('main');
 const searchCity = document.getElementById('#searchCity');
+const degCelsius = document.querySelector("sup").innerText;
 
-// const apiKey = "a6e755dd586961cb6abab58d3c3adb141"
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=Berlin&units=metric`;
-
+// const apiKey = "a6e755dd586961cb6abab58d3c3adb14";
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=islamabad&units=metric`;
 
 const checkWeather = async () => {
-    const request = await fetch(apiURL + `&appid=${apiKey}&`);
-    const data = await request.json();
-    if(!data) {
-        console.log(`Error`);
-    }
+    const requestWeather = await fetch(apiURL + `&appid=${apiKey}`);
+    let weatherData = await requestWeather.json();
 
-    console.log(data);
+
+    document.getElementById("icon-img").src = `https://openweathermap.org/img/wn/${weatherData["weather"][0]["icon"]}@2x.png `;
+    document.querySelector(".main-card h2").innerText = `${parseFloat(weatherData["main"]["temp"]) + ` ` + degCelsius}`;
+    let description = document.getElementById("#other-description");
+    description.innerText = `${weatherData["weather"][0]["description"]}`;
+    document.getElementById("temp-description").innerText = `${weatherData["weather"][0]["main"]},${description}`; get
+
+    console.log(weatherData);
 }
 
-checkWeather().then((data) => {
-    data;
-}).catch((err) => {
-    console.log(`Unauthorized api Key: ${err.message}`); 
-}); 
-
+checkWeather();
