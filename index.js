3d4072fc67f1bf24539043ddde37c57783cf918d
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-06-04 02:28:24 +0100

    changed the UI/UX of the main card

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const main = document.querySelector('main');
 const searchCity = document.getElementById('#searchCity');
-const degCelsius = document.querySelector("sup").innerText;
 
 // const apiKey = "a6e755dd586961cb6abab58d3c3adb14";
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=islamabad&units=metric`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=Berlin&units=metric`;
 
-const checkWeather = async () => {
-    const requestWeather = await fetch(apiURL + `&appid=${apiKey}`);
+const checkWeather = async() => {
+    const requestWeather = await fetch(apiURL+`&appid=${apiKey}`);
     let weatherData = await requestWeather.json();
 
-
-    document.getElementById("icon-img").src = `https://openweathermap.org/img/wn/${weatherData["weather"][0]["icon"]}@2x.png `;
-    document.querySelector(".main-card h2").innerText = `${parseFloat(weatherData["main"]["temp"]) + ` ` + degCelsius}`;
-    let description = document.getElementById("#other-description");
-    description.innerText = `${weatherData["weather"][0]["description"]}`;
-    document.getElementById("temp-description").innerText = `${weatherData["weather"][0]["main"]},${description}`; get
+    document.getElementById("icon-img").src = `https://openweathermap.org/img/wn/${weatherData["weather"][0]["icon"]}@2x.png`;
+    document.querySelector(".temp h2").innerText = `${weatherData["main"]["temp"]}\u2103`
+    document.getElementById("temp-description span").innerText = `${weatherData["weather"][0]["main"]}`
 
     console.log(weatherData);
 }
 
-checkWeather();
+// checkWeather();
