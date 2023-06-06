702972ab41006f65408ae90d1ff8ff5aa59fc8c1
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-06-07 00:04:52 +0100

    edit the markup and script

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const main = document.querySelector('main');
-const searchCity = document.getElementById('#searchCity');
+"use strict"
 
 const apiKey = "a6e755dd586961cb6abab58d3c3adb14";
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=munich&units=metric`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?&q=abuja&units=metric`;
 
-const checkWeather = async () => {
+async function checkWeather() {
+
   const requestWeather = await fetch(apiURL + `&appid=${apiKey}`);
   let weatherData = await requestWeather.json();
 
-  document.getElementById("icon-img").src = `https://openweathermap.org/img/wn/${weatherData["weather"][0]["icon"]}@2x.png`;
-  document.getElementById("temp").innerText = `${Math.round(weatherData["main"]["temp"])}\u2103`;
-  document.getElementById("main-weather").innerText += `${weatherData["weather"][0]["main"]},`;
-  document.getElementById("other-weather").innerText = `${weatherData["weather"][0]["description"]}`;
-  document.getElementById("city").innerText = `${weatherData["name"]},`;
-  document.getElementById("country").innerText = `${weatherData["sys"]["country"]}`;
-  document.querySelector(".pressure h4").innerText = `${weatherData["main"]["pressure"]} hPa`;
-  
+  const { clouds, coord, main, name, sys, visibility, weather, wind } = weatherData;
 
-  //dates
+  document.getElementById("icon-img").src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+  document.querySelector(".temp").innerText = `${Math.round(main.temp)}°C`;
+  document.getElementById("main-weather").innerText = `${weather[0].main},`;
+  document.getElementById("other-weather").innerText = weather[0].description;
+  document.getElementById("city").innerText = name;
+  document.getElementById("country").innerText = sys.country;
+  document.querySelector(".pressure h4").innerText = main.pressure + "hPa";
+  document.querySelector(".visibility h4").innerText = visibility + "m";
+  document.querySelector(".wind-speed span").innerText = wind.speed;
+  document.querySelector(".wind-degree span").innerText += wind.deg;
+
+  //date and time
 
   let date = new Date();
   const nameOfMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -27,9 +31,24 @@ const checkWeather = async () => {
   document.getElementById("year").innerText = date.getFullYear() + '. ';
   document.getElementById("time").innerText = `${date.getHours()}:${date.getMinutes()}`;
 
-  //second card 
 
-  // const canvas = document.querySelector("canvas");
+
+//get sunrise and sunset
+
+  function getSunRise(value) {
+    const timestamp = value;
+    const date = new Date(timestamp * 1000); // Multiply by 1000 to convert from seconds to milliseconds
+    const result = date.getHours() + ':' + date.getMinutes();
+    console.log(result);
+    return result;
+  }
+
+ document.querySelector(".sun-rise").innerText = getSunRise(sys["sunrise"]);
+ document.querySelector(".sun-set").innerText = getSunRise(sys["sunset"]);
+
+
+//canvas humidity, clouds feels-like
+  
   function draw(canva, text, value = 60) {
     const canvas = document.querySelector(canva);
     if (canvas.getContext) {
@@ -43,39 +62,45 @@ const checkWeather = async () => {
       const centerX = canvasWidth / 2;
       const centerY = canvasHeight / 2;
 
-      // Set the text properties
       ctx.font = "bold 24px Arial";
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
       ctx.fillStyle = "#fff"
 
-      // Calculate the text position
+      // set the text position
       const textX = centerX;
       const textY = centerY;
 
       // Draw the text at the center
       ctx.fillText(text, textX, textY);
 
-
       ctx.beginPath();
-      ctx.lineWidth = "10";
+      ctx.lineWidth = "6";
       ctx.lineCap = "butt";
       ctx.strokeStyle = "#fff";
-      ctx.arc(75, 75, 70, 0, (2 * Math.PI * value) / 100);
+      ctx.arc(50, 50, 45, 0, (2 * Math.PI * value) / 100);
       ctx.stroke();
 
     }
-    else
-    {
+    else {
       alert(`Your browser does not support <canvas> element`);
     }
   }
 
-  draw(".circle1", Math.round(weatherData["main"]["humidity"]) + "%", weatherData["main"]["humidity"]);
-  draw(".circle2", Math.round(weatherData["main"]["feels_like"]) + "%", weatherData["main"]["feels_like"]);
-  draw(".circle3", Math.round(weatherData["clouds"]["all"]) + "%", weatherData["clouds"]["all"]);
+  draw(".circle1", Math.round(main.humidity) + "%", main.humidity);
+  draw(".circle2", Math.round(main.feels_like) + "%", main.feels_like);
+  draw(".circle3", Math.round(clouds.all) + "%", clouds.all);
+
+  //wind
+
+  //others
+  document.querySelector(".longitude span").innerText = coord.lon;
+  document.querySelector(".latitude span").innerText += coord.lat;
+  document.querySelector(".min-temp span").innerText += main.temp_min + "°C";
+  document.querySelector(".max-temp span").innerText += main.temp_max + "°C";
+
   console.log(weatherData);
 }
 
-setTimeout(() => checkWeather(), 3000);
-// checkWeather(); 
+// setTimeout(() => checkWeather(), 3000);
+checkWeather(); 
